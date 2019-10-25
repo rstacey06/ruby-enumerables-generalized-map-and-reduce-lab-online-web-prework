0b67f217e2af4b17e658fdbf8d74978e45c920ad
burbage stacey <rstacey06@gmail.com>
2019-10-25 16:21:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,17 @@ def my_own_map(array)
   return new
 end
 
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
